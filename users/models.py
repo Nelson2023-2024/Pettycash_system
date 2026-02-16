7c99188be960531d11dc108b86b81627926d3cfa
Nelson2023-2024 <nelsonobuya19@gmail.com>
2026-02-16 22:48:36 +0300

    feat(auth): added role and status FK on the users table also the role and status during createsuperuser

--- users/models.py
+++ users/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
-from base.models import BaseModel
+from base.models import BaseModel, GenericBaseModel, Status
 from django.utils.translation import gettext_lazy as _
+
+class Role(GenericBaseModel):
+    code = models.CharField(max_length=20,unique=True)
+
+    class Meta:
+        verbose_name = _('Role')
+        verbose_name_plural = _('Roles')
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
 class UserManager(BaseUserManager):
@@ -9,6 +19,23 @@ class UserManager(BaseUserManager):
         # validation error when we dont pass email later
         if not email: raise ValueError("Users must have an email address")
         email = self.normalize_email(email) # Normalize the email address by lowercasing the domain part of it.
+
+        #auto assign a status if not provided
+        if 'status' not in extra_fields:
+            status,_ = Status.objects.get_or_create(
+                code ="ACT",
+                defaults={'name':'Active', 'description': 'Active user'}
+            )
+            extra_fields['status'] = status
+
+        # Auto-assign a default Role if not provided
+        if 'role' not in extra_fields:
+            role, _ = Role.objects.get_or_create(
+                code = 'EMP',
+                defaults={'name':'Employee', 'description': 'Standard user'}
+            )
+            extra_fields['role'] = role
+
         # It refers to the model that this manager is attached to (your custom User model). This way, if you rename your User class, this code doesn't break.
         user = self.model(email=email, **extra_fields) # email + all other fields like password
         user.set_password(password) # hash_password
@@ -18,37 +45,52 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+
+        # Superusers get an ADMIN role
+        if 'role' not in extra_fields:
+            role, _ = Role.objects.get_or_create(
+                code='ADM',
+                defaults={'name': 'Admin', 'description': 'Administrator'}
+            )
+            extra_fields['role'] = role
         return self.create_user(email,password, **extra_fields)
 
+
+
+
 class User(BaseModel, AbstractBaseUser, PermissionsMixin):
-    class Role(models.TextChoices):
-        EMPLOYEE = 'EMP', _('EMPLOYEE')
-        ADMIN = 'ADM', _('ADMIN')
-        FINANCE_OFFICER = 'FO', _('FINANCE OFFICER')
-        CHIEF_FINANCE_OFFICER = 'CFO', _('Chief Finance Officer')
-
-    class Status(models.TextChoices):
-        ACTIVE = 'ACT', _('Active')
-        ON_LEAVE = 'LEAVE', _('On Leave')
-        INACTIVE = 'INACT', _('Inactive')
-
     email = models.EmailField(unique=True, verbose_name=_('Email'))
     first_name = models.CharField(max_length=150, blank=True, verbose_name=_('First name'))
     last_name = models.CharField(max_length=150, blank=True, verbose_name=_('Last name'))
     other_name = models.CharField(max_length=150, blank=True, null=True, verbose_name=_('Other name'))
     phone_number = models.CharField(max_length=20, blank=True, verbose_name=_('Phone Number'))
     national_id = models.CharField(max_length=150, blank=True, null=True, verbose_name=_('National Identity Number'))
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_active = models.BooleanField(default=True)
-    status = models.CharField(max_length=15,choices=Status, default=Status.ACTIVE)
-    role = models.CharField(max_length=15,choices=Role, default=Role.EMPLOYEE)
-    is_staff = models.BooleanField(default=True)
     avatar_url = models.CharField(max_length=500, blank=True, null=True)
+
+    last_login = models.DateTimeField(blank=True, null=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+
+
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.PROTECT,
+        related_name='users',
+        verbose_name='Status',
+
+    )
+    role = models.ForeignKey(
+        Role,
+        on_delete=models.PROTECT,
+        related_name='users',
+        verbose_name='Role'
+    )
 
     # default manager for this class
     objects = UserManager()
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['']
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.first_name+" "+self.last_name
