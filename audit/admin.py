c5efc280b7dfe01a502f64b839a7b557dfde5898
Nelson2023-2024 <nelsonobuya19@gmail.com>
2026-02-17 11:15:49 +0300

    feat(admin): register Category model in admin panel

--- audit/admin.py
+++ audit/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from audit.models import TransactionLogBase,Notifications,EventTypes,EventCategory
+from audit.models import TransactionLogBase,Notifications,EventTypes
 
 # Register your models here.
-admin.site.register(EventCategory)
+# admin.site.register(EventCategory)
 admin.site.register(EventTypes)
 admin.site.register(TransactionLogBase)
 admin.site.register(Notifications)
--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from base.models import Status
+from base.models import Status, Category
 
 # Register your models here.
 
 admin.site.register(Status)
+admin.site.register(Category)
