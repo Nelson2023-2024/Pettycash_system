1e0668c98d0845d0f6a4e3fa80d9554b0575e75a
Nelson2023-2024 <nelsonobuya19@gmail.com>
2026-02-19 08:46:04 +0300

    feat(auth): implemented basic AuthService to handle auth operations and called them in the views

---
+++ authenticate/views.py
@@ -0,0 +1,20 @@
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
+
+from utils.response_provider import ResponseProvider
+from authenticate.services.auth_services import AuthService
+
+# Create your views here.
+# @require_http_methods(['POST'])
+@csrf_exempt
+def login(request) -> JsonResponse:
+    try:
+        user_data = AuthService.login(request)
+        return ResponseProvider.success(message="Login successful", data=user_data)
+    except Exception as ex:
+        return ResponseProvider.handle_exception(ex)
+
+def logout(request) -> JsonResponse:
+    AuthService.logout(request.user)
+    return ResponseProvider.success(message="Logout successful")
