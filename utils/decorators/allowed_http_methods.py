15a84586fc056ddb75c41c85afb5dd216bb0acbc
Nelson2023-2024 <nelsonobuya19@gmail.com>
2026-02-20 08:12:15 +0300

    feat(middleware): implement allowed_http_methods decorator to restrict unsupported HTTP methods

---
+++ utils/decorators/allowed_http_methods.py
@@ -0,0 +1,16 @@
+from functools import wraps
+from utils.response_provider import ResponseProvider
+
+def allowed_http_methods(allowed_methods=None):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(request, *args, **kwargs):
+            if allowed_methods and request.method not in allowed_methods:
+                return ResponseProvider().bad_request(
+                    message='Method not allowed',
+                    error=f"{request.method} method is not allowed. Allowed methods: {','.join(allowed_methods)}"       
+                )
+            return func(request, *args, **kwargs)
+        
+        return wrapper
+    return decorator
