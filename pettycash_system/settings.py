7bd2b5dc10b0f5d567617595e25eb08625e50586
Nelson2023-2024 <nelsonobuya19@gmail.com>
2026-03-31 22:17:25 +0300

    chore(config): update allowed hosts for Docker environment
    - Add backend service names and localhost entries
    - Support containerized deployment and scaling

--- pettycash_system/settings.py
+++ pettycash_system/settings.py
@@ -39,7 +39,14 @@ SECRET_KEY = "django-insecure-wp+03--cs*3$d7_%@95#!1k_(ezv-h_2(vytn(kh2$rd#g022y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "pettycash-backend",   # Docker service name
+    "pettycash-backend-1", # Scaled instance 1
+    "pettycash-backend-2", # Scaled instance 2
+    "0.0.0.0",
+]
 
 AUTH_USER_MODEL = "users.User"
 # Application definition
